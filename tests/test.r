48625b1ae93884dee7c4ef90b07977670cb912f7
Lam Ho <lamho86@gmail.com>
2014-10-23 16:26:17 -0700

    Rcpp version

--- tests/test.r
+++ tests/test.r
@@ -10,7 +10,7 @@ library(Rcpp)
 
 ### Within-host macroparasite population
 
-a0 = 30
+a0 = 100
 b0 = 0
 A = 0
 B = a0
@@ -26,8 +26,8 @@ drates2=function(a,b){muM*b}
 trans=function(a,b){gamma*a} # a -> b
 
 #Rprof("func.out",memory.profiling=T)
-system.time(p <- dbd_prob(t=1,a0,b0,drates1,brates2,drates2,trans,a=A,B))
-#p <- dbd_prob(t=400,a0,b0,drates1,brates2,drates2,trans,a=A,B)
+#system.time(p <- dbd_prob(t=1,a0,b0,drates1,brates2,drates2,trans,a=A,B))
+p <- dbd_prob(t=400,a0,b0,drates1,brates2,drates2,trans,a=A,B)
 #Rprof(NULL)
 #summaryRprof("func.out",memory="both")
 sum(p)
