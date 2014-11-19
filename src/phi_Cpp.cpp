16e16a5f61ef9c9d746ca79432455297b99a4318
Marc Suchard <msuchard@gmail.com>
2014-11-19 14:25:14 -0800

    Unit tests

--- src/phi_Cpp.cpp
+++ src/phi_Cpp.cpp
@@ -3,7 +3,12 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-void phi_Cpp (const std::complex<double> s, const int a0, const int b0, const std::vector<double>& lambda2, const std::vector<double>& mu2, const std::vector<double>& x, const std::vector<double>& y, const int A, const int B, const int maxdepth, std::vector<std::complex<double>>& phi, const std::deque<std::vector<double>>& prod_mu2, const std::deque<std::vector<double>>& prod_lambda2) {  
+void phi_Cpp (const std::complex<double> s, const int a0, const int b0, 
+    const std::vector<double>& lambda2, const std::vector<double>& mu2,  // TODO Not used
+    const std::vector<double>& x, const std::vector<double>& y, 
+    const int A, const int B, const int maxdepth, 
+    std::vector<std::complex<double>>& phi, 
+    const std::deque<std::vector<double>>& prod_mu2, const std::deque<std::vector<double>>& prod_lambda2) {  
   
   const int dim = B+1, dimsq = (B+1)*(B+1);
   const std::complex<double> one(1.0,0.0);
@@ -18,13 +23,15 @@ void phi_Cpp (const std::complex<double> s, const int a0, const int b0, const st
         
         lentz = lentz_Cpp(B,xvec,yvec);
         inv_Bk1dBk = inv_Bk1dBk_Cpp(B,xvec,yvec);
-        BidBj = BidBj_Cpp(B,xvec,yvec,inv_Bk1dBk);
+        BidBj = BidBj_Cpp(B,xvec,yvec,inv_Bk1dBk); // TODO Compute in place
         
         for (int i=0; i<=B; ++i) 
         	for (int j=0; j<=B; ++j) {
 			      if (i==j) phi[a*dimsq + i*dim + j] = BidBj[i*dim + j]/(inv_Bk1dBk[j]+lentz[j]);
             if (i<j) phi[a*dimsq + i*dim + j] = prod_mu2[a][(i+1)*dim + j]*BidBj[i*dim + j]/(inv_Bk1dBk[j]+lentz[j]);
 				    if (i>j) phi[a*dimsq + i*dim + j] = prod_lambda2[a][j*dim + i-1]*BidBj[j*dim + i]/(inv_Bk1dBk[i]+lentz[i]);
+            // TODO prod_mu2, prod_lambda2 as flat vector
+            // TODO flat iterator over triangular space                    
 		    }
   }
 }
