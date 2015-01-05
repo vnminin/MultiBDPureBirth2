e73f60057e296b8844d66e54fae66b260ff8d555
Lam Ho <lamho86@gmail.com>
2015-01-05 12:30:41 -0800

    clean up code

--- src/BidBj_Cpp.cpp
+++ src/BidBj_Cpp.cpp
@@ -2,9 +2,7 @@
 
 void BidBj_Cpp(const int Bp1, const std::vector<double>& xvec, const mytype::ComplexVector& yvec, 
     const mytype::ComplexVector& inv_Bk1dBk, mytype::ComplexVector& BidBj) {
-  
-  int flag;
-  
+    
   std::for_each(boost::make_counting_iterator(0), boost::make_counting_iterator(Bp1-1),
 //  unroll::for_each_4(boost::make_counting_iterator(0), boost::make_counting_iterator(Bp1-1),
     [&](int i) {
