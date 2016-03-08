f2be6b8fc4409d4d09fcb89370690ec786b2b2b2
Marc Suchard <msuchard@gmail.com>
2016-03-08 15:20:01 -0800

    one example; many more to go...

--- R/dbd_prob.R
+++ R/dbd_prob.R
@@ -20,8 +20,39 @@
 #' @param computeMode computation mode
 #' @param nThreads number of threads
 #' @param maxdepth maximum number of iterations for Lentz algorithm
+#' 
+#' 
+#' @examples
+#' data(Eyam)
+#' 
+#' loglik_sir <- function(param, data) {
+#'   alpha <- exp(param[1]) # Rates must be non-negative
+#'   beta  <- exp(param[2])
+#'   
+#'   # Set-up SIR model
+#'   drates1 <- function(a, b) { 0 }
+#'   brates2 <- function(a, b) { 0 }
+#'   drates2 <- function(a, b) { alpha * b     }
+#'   trans12 <- function(a, b) { beta  * a * b }
+#'   
+#'   sum(sapply(1:(nrow(data) - 1), # Sum across all time steps k
+#'              function(k) {
+#'                log(
+#'                  dbd_prob(  # Compute the transition probability matrix
+#'                    t  = data$time[k + 1] - data$time[k], # Time increment
+#'                    a0 = data$S[k], b0 = data$I[k],       # From: S(t_k), I(t_k)                                      
+#'                    drates1, brates2, drates2, trans12,
+#'                    a = data$S[k + 1], B = data$S[k] + data$I[k] - data$S[k + 1],
+#'                    computeMode = 4, nblocks = 80         # Compute using 4 threads
+#'                  )[1, data$I[k + 1] + 1]                 # To: S(t_(k+1)), I(t_(k+1))
+#'                )
+#'              }))
+#' }
+#' 
+#' loglik_sir(log(c(3.204, 0.019)), Eyam) # Evaluate at mode
+#' 
 #' @export
-dbd_prob <-function(t, a0, b0, mu1, lambda2, mu2, gamma, a=0, B,
+dbd_prob <- function(t, a0, b0, mu1, lambda2, mu2, gamma, a=0, B,
                     nblocks=256, tol=1e-12, computeMode=0, nThreads=4,
                     maxdepth=400) {
   
