81323ac8880f58b7790c3bd6b0d4c5aa03ba91d2
Marc Suchard <msuchard@gmail.com>
2016-07-19 10:56:49 -0700

    fix imports

--- R/MultiBD.R
+++ R/MultiBD.R
@@ -1,12 +1,14 @@
 #' Multivariate birth-death processes
-#' 
-#' The MultiBD package computes the transition probabilities 
+#'
+#' The MultiBD package computes the transition probabilities
 #' of several multivariate birth-death processes.
-#' 
+#'
 #' @references Ho LST et al. 2016. "Birth(death)/birth-death processes and their computable transition probabilities with statistical applications". In review.
-#' 
+#'
 #' @docType package
 #' @name MultiBD
-#' @import Rcpp RcppParallel BH
+#' @import BH
+#' @importFrom RcppParallel RcppParallelLibs
+#' @importFrom Rcpp evalCpp
 #' @useDynLib MultiBD
 NULL
--- cran-comments.md
+++ cran-comments.md
@@ -1,17 +1,10 @@
 ## Test environments
-* local OS X install, R 3.1.2
-* ubuntu 12.04 (on travis-ci), R 3.1.2
+* local OS X install, R 3.2.2
+* ubuntu 12.04 (on travis-ci), R 3.1.2 XXX
 * win-builder (devel and release)
 
 ## R CMD check results
-There were no ERRORs or WARNINGs. 
-
-There was 1 NOTE:
-
-* checking dependencies in R code ... NOTE
-  Namespace in Imports field not imported from: 'R6'
-
-  R6 is a build-time dependency.
+There were no ERRORs, WARNINGs or NOTEs.
 
 ## Downstream dependencies
 There are currently no downstream dependencies.
